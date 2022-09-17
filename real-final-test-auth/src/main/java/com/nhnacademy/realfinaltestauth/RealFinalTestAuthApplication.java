009efcddc9aabcfc6fbeadbba4b7a1251c859e72
조재철 <rhsnqk@gmail.com>
2022-09-18 00:13:02 +0900

    Create Spring Boot Project with Maven

---
+++ real-final-test-auth/src/main/java/com/nhnacademy/realfinaltestauth/RealFinalTestAuthApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.realfinaltestauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealFinalTestAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealFinalTestAuthApplication.class, args);
+	}
+
+}
